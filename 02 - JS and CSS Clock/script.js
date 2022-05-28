e9787603f9dd4e476a2cc1a9d477bb6e0753ad0b
Henry <hnguyhenry@protonmail.com>
2022-05-27 17:52:06 -0700

    Add script.js to index-start.html

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const setDate = () => {
+    const current = new Date();
+
+    const seconds = current.getSeconds();
+    const minutes = current.getMinutes();
+    const hours = current.getHours();
+
+    console.log("Hours: " + hours);
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    
+
+    console.log(seconds);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+}
+
+setInterval(setDate, 1000);
